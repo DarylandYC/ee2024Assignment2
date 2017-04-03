40d89a3ffd4e824f1239bc101f69f91130d9019c
yongchenglow <lowyongcheng@hotmail.com>
2017-04-03 21:46:27 +0800

    Update Master
    Invert 7 Seg
    Implement Stable and Monitor Mode

--- Assignment2/Debug/src/cr_startup_lpc17.d
+++ Assignment2/Debug/src/cr_startup_lpc17.d
@@ -1,4 +1,4 @@
 src/cr_startup_lpc17.d: ../src/cr_startup_lpc17.c \
- C:\Users\Daryl\Desktop\ee2024Assignment2\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h
+ C:\Users\yongchenglow\Documents\LPCXpresso_6.1.4_194\workspace\ee2024Assignment2\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h
 
-C:\Users\Daryl\Desktop\ee2024Assignment2\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
+C:\Users\yongchenglow\Documents\LPCXpresso_6.1.4_194\workspace\ee2024Assignment2\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
--- Lib_MCU/Debug/src/lpc17xx_libcfg_default.d
+++ Lib_MCU/Debug/src/lpc17xx_libcfg_default.d
@@ -1,7 +1,7 @@
 src/lpc17xx_libcfg_default.d: ../src/lpc17xx_libcfg_default.c \
- C:\Users\Daryl\Desktop\ee2024Assignment2\Lib_MCU\inc/lpc17xx_libcfg_default.h \
- C:\Users\Daryl\Desktop\ee2024Assignment2\Lib_MCU\inc/lpc_types.h
+ C:\Users\yongchenglow\Documents\LPCXpresso_6.1.4_194\workspace\ee2024Assignment2\Lib_MCU\inc/lpc17xx_libcfg_default.h \
+ C:\Users\yongchenglow\Documents\LPCXpresso_6.1.4_194\workspace\ee2024Assignment2\Lib_MCU\inc/lpc_types.h
 
-C:\Users\Daryl\Desktop\ee2024Assignment2\Lib_MCU\inc/lpc17xx_libcfg_default.h:
+C:\Users\yongchenglow\Documents\LPCXpresso_6.1.4_194\workspace\ee2024Assignment2\Lib_MCU\inc/lpc17xx_libcfg_default.h:
 
-C:\Users\Daryl\Desktop\ee2024Assignment2\Lib_MCU\inc/lpc_types.h:
+C:\Users\yongchenglow\Documents\LPCXpresso_6.1.4_194\workspace\ee2024Assignment2\Lib_MCU\inc/lpc_types.h:
